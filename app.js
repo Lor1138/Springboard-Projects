7b4dc061ffeba4041edd71953f43d95962d8f778
Laura OBrien <lauraobdev@gmail.com>
2021-10-14 19:25:11 -0400

    Added new files to correct location

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-console.log("Let's get this party started!");
+
 
 //Add all page elements with ids that are needed to collect and submit values
-const searchInput = document.getElementById('#search');
-const submitBtn  = document.getElementById('#submit');
-const removeBtn = document.getElementById('#delete');
-const gifContainer = document.getElementById('#gif-container')
+const searchInput = document.getElementById('search');
+const submitBtn  = document.getElementById('submit');
+const removeBtn = document.getElementById('delete');
+const gifContainer = document.getElementById('gif-container')
+const inputData = searchInput.value;
 
-/*
-searchInput.addEventListener(function(e){
+
+submitBtn.addEventListener('click', function(e){
     e.preventDefault();
     fetchGif();
     searchInput.value = " ";
 });
 
-removeBtn.addEventListener(function(e){
-    img.remove();
+removeBtn.addEventListener('click', function(e){
+    e.preventDefault();
+    $('img').remove();
 });
-*/
+
 async function fetchGif(){
-    const res = await axios.get('api.giphy.com/v1/gifs/search?q=`${searchInput}`&api_key=I9k7dN8ybyxlyER5yc30YYTvNskJ2HAm&limit=25');
-        console.log(res)
+    try{
+        const res = await axios.get(`https://api.giphy.com/v1/gifs/search?&q=${inputData}&api_key=I9k7dN8ybyxlyER5yc30YYTvNskJ2HAm`);
+        const random = Math.random(Math.floor() * res.data.length)
+        const urlGif = res.data.data[random].images.downsized.url;
+        createGif();
+    }
+    catch (e) {
+        alert("No results, try another search term")
+    }
 }
+
+function createGif(url){
+    const createImg = document.createElement('img');
+    createImg.setAtttribute('src', urlGif);
+    gifContainer.append(createImg);
+};
 
